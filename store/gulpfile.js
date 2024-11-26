16a59482b6c35e3116a6b6e8bae3ccb66b2ef942
Erislandio Soares <erislandiosoares@MacBook-Pro-de-Erislandio.local>
2024-11-26 14:41:34 -0300

    fix: fix

--- store/gulpfile.js
+++ store/gulpfile.js
@@ -2,11 +2,13 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const path = require('path')
 
+// Caminhos dos arquivos SCSS e destino do CSS
 const scssFiles = './styles/scss/**/*.scss'
 const cssDest = './styles/css'
 
+// Função para compilar um único arquivo SCSS
 function compileSingleFile(filePath) {
-  // Extrai o nome do diretório relativo do arquivo modificado
+  // Extrai o caminho relativo do arquivo modificado
   const relativePath = path.relative(
     path.resolve('styles/scss'),
     path.dirname(filePath)
@@ -14,16 +16,38 @@ function compileSingleFile(filePath) {
 
   return gulp
     .src(filePath) // Somente o arquivo alterado
-    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      sass().on('error', function (err) {
+        console.error('Erro de compilação do SASS:', err.message)
+        this.emit('end')
+      })
+    )
     .pipe(gulp.dest(path.join(cssDest, relativePath))) // Salva o CSS mantendo a estrutura
+    .on('end', () => console.log(`Compilado: ${filePath}`))
 }
 
+// Função para compilar todos os arquivos SCSS
+function compileAll() {
+  return gulp
+    .src(scssFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(cssDest))
+    .on('end', () => console.log('Todos os arquivos SCSS foram compilados.'))
+}
+
+// Função para monitorar mudanças nos arquivos SCSS
 function watch() {
-  gulp.watch(scssFiles).on('change', function (filePath) {
-    console.log(`Arquivo modificado: ${filePath}`)
-    compileSingleFile(filePath) // Compila somente o arquivo modificado
-  })
+  gulp
+    .watch(scssFiles, function (cb) {
+      compileAll()
+      cb()
+    })
+    .on('change', function (filePath) {
+      console.log(`Arquivo modificado: ${filePath}`)
+      compileSingleFile(filePath)
+    })
 }
 
+// Exporta as tarefas do Gulp
+exports.compileAll = compileAll
 exports.watch = watch
-exports.mount = watch
